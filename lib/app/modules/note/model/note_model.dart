b44cc72ec7d338a216dbb586ebadcea562d6c87b
AlexisMrcx <alexis.marcoux@live.fr>
2021-07-06 16:24:30 +0200

    Ajout du modele de note

---
+++ lib/app/modules/note/model/note_model.dart
@@ -0,0 +1,8 @@
+class Note {
+  final String title;
+  final DateTime dateTime;
+  final String content;
+  final String picturePath;
+
+  Note(this.title, this.dateTime, this.content, this.picturePath);
+}
